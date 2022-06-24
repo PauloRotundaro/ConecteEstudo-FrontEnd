d8548cee522daac24e2cf88fdc0e37a0e0c74eeb
Paulo Rotundaro <paulo.rotundaro@toodoo.com.br>
2022-06-24 09:22:09 -0300

    feat: class details

--- conecte-estudo/src/Components/ClassDetails.js
+++ conecte-estudo/src/Components/ClassDetails.js
@@ -3,38 +3,41 @@ import Header from "./Header";
 import "../Styles/ClassDetails.css";
 import AddLesson from "../Components/Modals/AddLesson.js";
 import AddWork from "../Components/Modals/AddWork.js";
-import axios from 'axios';
+import axios from "axios";
 
 export default class ClassDetails extends Component {
-    componentDidMount() {
-        const pathname = window.location.pathname;
-        const windowClassId = pathname.split('/')[2];
 
-        const classId = windowClassId;
+    state = {
+        userId: 0,
+        classId: 0,
+        classDetails: {},
+        classMembers: 0
+    }
+
+    componentDidMount() {
+        const userdata = JSON.parse(localStorage.getItem('user'));
+        const userId = userdata[0].userId;
+        this.setState({ userId });
+
+        const { classId } = this.props.match.params;
+        localStorage.setItem('classId', classId);
+        this.setState({ classId });
 
         axios.get(`http://127.0.0.1:8000/classroom/` + classId)
             .then(res => {
-                localStorage.setItem('class', JSON.stringify(res.data));
-            })
-
-        var config = {
-            method: 'get',
-            url: 'http://127.0.0.1:8000/userClassroom/' + classId,
-            headers: {}
-        };
-
-        axios(config)
-            .then(function (response) {
-                console.log(JSON.stringify(response.data));
-
-                const usersInClass = response.data[0].user
-                console.log(usersInClass)
-
-                localStorage.setItem('usersInClass', usersInClass)
-            })
-            .catch(function (error) {
-                console.log(error);
+                const classDetails = res.data[0];
+                const classMembers = Object.keys(classDetails.user).length;
+                this.setState({ classDetails });
+                this.setState({ classMembers });
             });
+    }
+
+    toTeachingPlan = (classId) => {
+        window.location.replace('/teaching-plan/' + classId)
+    }
+
+    toTeacherFrequency = () => {
+        window.location.replace('/class-frequency/' + this.state.classId)
     }
 
     render() {
@@ -46,15 +49,15 @@ export default class ClassDetails extends Component {
                         <span className="title mg-20">Detalhes da turma</span>
                         <div className="detailItem">
                             <span className="detailItemTitle">Membros da equipe: </span>
-                            <div className="classMembers">64</div>
+                            <div className="classMembers">{this.state.classMembers}</div>
                         </div>
                         <div className="detailItem">
                             <span className="detailItemTitle">Turma: </span>
-                            <div className="className">C202 - A</div>
+                            <div className="className">{this.state.classDetails.className}</div>
                         </div>
                         <div className="detailItem">
                             <span className="detailItemTitle">Frequência: </span>
-                            <a href="/class-frequency" className="classDetailsButton">Atribuir frequência</a>
+                            <button onClick={this.toTeacherFrequency} className="classDetailsButton">Atribuir frequência</button>
                         </div>
                         <div className="detailItem">
                             <span className="detailItemTitle">Agendar aula: </span>
@@ -63,6 +66,10 @@ export default class ClassDetails extends Component {
                         <div className="detailItem">
                             <span className="detailItemTitle">Atribuir atividade: </span>
                             <AddWork />
+                        </div>
+                        <div className="detailItem">
+                            <span className="detailItemTitle">Plano de ensino: </span>
+                            <button onClick={event => this.toTeachingPlan(this.state.classId)} className="classDetailsButton">Ver plano</button>
                         </div>
                     </div>
                 </div>
